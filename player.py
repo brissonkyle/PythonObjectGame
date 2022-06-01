3fe0695aae5d0e176f869c37f464d66ab058cbf6
brissonkyle <98568756+brissonkyle@users.noreply.github.com>
2022-06-01 15:26:55 -0600

    Finished Python Object Game

---
+++ player.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self, initialRow, initialColumn):
+        self.rowPosition = initialRow
+        self.columnPosition = initialColumn
+
+    # TODO
+    def moveUp(self):
+        self.rowPosition -= 1
+
+    # TODO
+    def moveDown(self):
+        self.rowPosition += 1
+    # TODO
+    def moveLeft(self):
+        self.columnPosition -= 1
+
+    # TODO
+    def moveRight(self):
+        self.columnPosition += 1
